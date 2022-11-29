535d62de96787bd54e2a573e72a7b68313bc8195
hiroshiimaizumi <hiroshiimaizumi0611@gmail.com>
2022-11-29 11:05:32 +0900

    register commandHandler

--- SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
@@ -4,6 +4,7 @@ using CQRS.Core.Infrastructure;
 using Post.Cmd.Api.Commands;
 using Post.Cmd.Domain.Aggregates;
 using Post.Cmd.Infrastructure.Config;
+using Post.Cmd.Infrastructure.Dispatchers;
 using Post.Cmd.Infrastructure.Handlers;
 using Post.Cmd.Infrastructure.Repositories;
 using Post.Cmd.Infrastructure.Stores;
@@ -16,6 +17,19 @@ builder.Services.AddScoped<IEventStoreRepository, EventStoreRepository>();
 builder.Services.AddScoped<IEventStore, EventStore>();
 builder.Services.AddScoped<ICommandHandler, CommandHandler>();
 builder.Services.AddScoped<IEventSourcingHandler<PostAggregate>, EventSourcingHandler>();
+
+// register commandHandler methods
+var commandHandler = builder.Services.BuildServiceProvider().GetRequiredService<ICommandHandler>();
+var dispatcher = new CommandDispatcher();
+dispatcher.RegisterHandler<NewPostCommand>(commandHandler.HandlerAsync);
+dispatcher.RegisterHandler<EditMessageCommand>(commandHandler.HandlerAsync);
+dispatcher.RegisterHandler<LikePostCommand>(commandHandler.HandlerAsync);
+dispatcher.RegisterHandler<AddCommentCommand>(commandHandler.HandlerAsync);
+dispatcher.RegisterHandler<EditCommentCommand>(commandHandler.HandlerAsync);
+dispatcher.RegisterHandler<RemoveCommentCommand>(commandHandler.HandlerAsync);
+dispatcher.RegisterHandler<DeletePostCommand>(commandHandler.HandlerAsync);
+builder.Services.AddSingleton<ICommandDispatcher>(_ => dispatcher);
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
