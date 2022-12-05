bcd526178ea752054cda29f89342beff55cb6cbe
hiroshiimaizumi <hiroshiimaizumi0611@gmail.com>
2022-12-05 15:52:55 +0900

    add removeCommentController

--- SM-Post/Post.Cmd/Post.Cmd.Api/Controllers/RemoveCommentController.cs
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Controllers/RemoveCommentController.cs
@@ -8,19 +8,19 @@ namespace Post.Cmd.Api.Controllers
 {
     [ApiController]
     [Route("api/v1/[controller]")]
-    public class EditMessageController : ControllerBase
+    public class RemoveCommentController : ControllerBase
     {
-        private readonly ILogger<EditMessageController> _logger;
+        private readonly ILogger<RemoveCommentController> _logger;
         private readonly ICommandDispatcher _commandDispatcher;
 
-        public EditMessageController(ILogger<EditMessageController> logger, ICommandDispatcher commandDispatcher)
+        public RemoveCommentController(ILogger<RemoveCommentController> logger, ICommandDispatcher commandDispatcher)
         {
             _logger = logger;
             _commandDispatcher = commandDispatcher;
         }
 
-        [HttpPut("{id}")]
-        public async Task<ActionResult> EditMessageAsync(Guid id, EditMessageCommand command)
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> RemoveCommentAsync(Guid id, RemoveCommentCommand command)
         {
             try
             {
@@ -29,7 +29,7 @@ namespace Post.Cmd.Api.Controllers
 
                 return Ok(new BaseResponse
                 {
-                    Message = "Edit massage request completed successfully"
+                    Message = "Remove comment request completed successfully!"
                 });
             }
             catch (InvalidOperationException ex)
@@ -50,7 +50,7 @@ namespace Post.Cmd.Api.Controllers
             }
             catch (Exception ex)
             {
-                const string SAFE_ERROR_MESSAGE = "Error while processing request to edit a the message of a post.";
+                const string SAFE_ERROR_MESSAGE = "Error while processing request to remove a the message from a post.";
                 _logger.Log(LogLevel.Error, ex, SAFE_ERROR_MESSAGE);
 
                 return StatusCode(StatusCodes.Status500InternalServerError, new BaseResponse
